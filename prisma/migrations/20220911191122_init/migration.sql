a7ed4284f6f7e9e7c9590374f4ff587e93aa7548
Arkadeion <arkadeion@gmail.com>
2022-09-11 21:12:24 +0200

    Add exercise 15

---
+++ prisma/migrations/20220911191122_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "userId" INTEGER NOT NULL,
+    "id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
